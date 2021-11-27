4e04d9420ddc7b93c57df21daf7b57db00cda970
Ujjwal Kumar <epicujjwal@protonmail.com>
2021-11-27 10:32:43 +0530

    nav: nav desktop

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,19 +3,24 @@ import styles from '../styles/nav.module.css'
 
 const Navbar = () => {
     return (
-        <nav>
+        <nav className={styles.nav}>
             <SimpleGrid columns={3} > 
                 <div className={styles.logo}>
-                  <strong>Hackarmour</strong>
+                <img src="./logo.svg" alt="logo" />
+                  <strong>
+                      HackArmour
+                  </strong>
                 </div>
 
-                <SimpleGrid columns={3} className={styles.links}>
-                    <div>GitHub</div>
-                    <div>Community</div>
-                    <div>About</div>
+                <SimpleGrid columns={3} spacing={10} className={`${styles.links} text-med`}>
+                    <a href="https://github.com/hackarmour">GitHub</a>
+                    <a>Community</a>
+                    <a>About</a>
                 </SimpleGrid>
 
-                <div className={styles.discord}>discord</div>
+                <a href="https://discord.gg/xBq4QSmjMp" className={`${styles.discord} text-med`}>
+                    discord
+                </a>
             </SimpleGrid>
         </nav>
     )
