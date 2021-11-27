9aead70b1b6cc1660c789a50cd16f0ad4280a5f4
Ujjwal Kumar <epicujjwal@protonmail.com>
2021-11-27 11:56:39 +0530

    nav: add media queries and js styles

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,29 +1,59 @@
 import { SimpleGrid } from '@chakra-ui/react'
-import styles from '../styles/nav.module.css'
+import styled from '@emotion/styled'
 
 const Navbar = () => {
     return (
-        <nav className={styles.nav}>
-            <SimpleGrid columns={3} > 
-                <div className={styles.logo}>
-                <img src="./logo.svg" alt="logo" />
-                  <strong>
-                      HackArmour
-                  </strong>
-                </div>
-
-                <SimpleGrid columns={3} spacing={10} className={`${styles.links} text-med`}>
-                    <a href="https://github.com/hackarmour">GitHub</a>
-                    <a>Community</a>
-                    <a>About</a>
-                </SimpleGrid>
-
-                <a href="https://discord.gg/xBq4QSmjMp" className={`${styles.discord} text-med`}>
-                    discord
-                </a>
+        <Nav>
+            <SimpleGrid columns={{sm: 1, lg: 3}} >
+                <Logo>
+                    <img src="./logo.svg" alt="logo" />
+                    <strong>
+                        HackArmour
+                    </strong>
+                </Logo>
+                <Links className="text-med">
+                    <SimpleGrid columns={3} spacing={10}>
+                        <a href="https://github.com/hackarmour">GitHub</a>
+                        <a>Community</a>
+                        <a>About</a>
+                    </SimpleGrid>
+                </Links>
+                <Discord href="https://discord.gg/xBq4QSmjMp" className="text-med">
+                    Discord
+                </Discord>
             </SimpleGrid>
-        </nav>
+        </Nav>
     )
 }
+
+const Nav = styled.nav`
+    padding: .5rem;
+    font-size: 1.12rem;
+`
+
+const Links = styled.div`
+    text-align: center;
+    margin: auto;
+    @media all and (max-width: 62rem) {
+        display: none !important;
+    }
+`
+
+const Discord = styled.a`
+    text-align: right;
+    margin: auto;
+    @media all and (max-width: 62rem) {
+        display: none !important;
+    }
+`
+
+const Logo = styled.div`
+    font-size: 1.4rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+
 
 export default Navbar
