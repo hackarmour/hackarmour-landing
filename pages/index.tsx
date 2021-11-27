46a775cfac2e8a62ecd356161a2d345f15238331
Ujjwal Kumar <epicujjwal@protonmail.com>
2021-11-27 12:59:38 +0530

    hero: add button and some style fixes -- test-1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Text } from "@chakra-ui/react"
+import { Text, Button } from "@chakra-ui/react"
 import styled from '@emotion/styled'
 import type { NextPage } from 'next'
 import Head from 'next/head'
@@ -18,13 +18,18 @@ const Home: NextPage = () => {
           <Text fontSize={{ lg: "7xl", sm: "5xl" }} className="bold">
             Welcome To Our  <br /> Community
           </Text>
-          <br />
+          <Break />
           <Text className="text" fontSize={{lg: "3xl"}}>
           We collect resources and make cool projects related to infosec. <br /> Currently building an Infosec search engine!
           </Text>
+          <br />
+          <Button
+           className="text-med"
+           backgroundColor="blue.400"
+           fontSize="1xl"
+          >Learn More</Button>
         </Container>
       </Hero>
-
     </div>
   )
 }
@@ -35,8 +40,14 @@ const Hero = styled.main`
   height: 90vh;
 `
 
+const Break = styled.br`
+  @media all and (max-width: 62rem) {
+    display: none;
+  }
+`
+
 const Container = styled.div`
-  padding-top: 20vh;
+  padding-top: 17vh;
   @media all and (max-width: 62rem) {
     paddding-top: 1vh;
   }
