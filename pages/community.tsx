0e97c11ffa4791aac413987674f65e2a91938a84
Ujjwal Kumar <epicujjwal@protonmail.com>
2021-11-29 12:56:05 +0530

    community: add discord link

--- pages/community.tsx
+++ pages/community.tsx
@@ -5,33 +5,35 @@ import { FadeInDown } from '../animations'
 import { Hero, Container, Heading, Break, Description } from '../styles/pageStyles'
 
 const Community: NextPage = () => {
-  return (
-    <div>
-      <Head>
-        <title>Hackarmour Community Page</title>
-        {/* TODO: Meta Tags */}
-      </Head>
-      <FadeInDown>
-        <Hero>
-          <Container>
-            <Heading className="bold-fira">
-              Our Community Page
-            </Heading>
-            <Break />
-            <Description className="text">
-              The community and contribution guidelines, and staff information.
-            </Description>
-            <br />
-            <Button
-              className="text-med"
-              backgroundColor="#2D5BFF"
-              fontSize={{ lg: "1.3rem", md: "1rem" }}
-            >Contact Us</Button>
-          </Container>
-        </Hero>
-      </FadeInDown>
-    </div>
-  )
+    return (
+        <div>
+            <Head>
+                <title>Hackarmour Community Page</title>
+                {/* TODO: Meta Tags */}
+            </Head>
+            <FadeInDown>
+                <Hero>
+                    <Container>
+                        <Heading className="bold-fira">
+                            Our Community Page
+                        </Heading>
+                        <Break />
+                        <Description className="text">
+                            The community and contribution guidelines, and staff information.
+                        </Description>
+                        <br />
+                        <a href="https://discord.gg/xBq4QSmjMp">
+                            <Button
+                                className="text-med"
+                                backgroundColor="#2D5BFF"
+                                fontSize={{ lg: "1.3rem", md: "1rem" }}
+                            > Join Discord</Button>
+                        </a>
+                    </Container>
+                </Hero>
+            </FadeInDown>
+        </div>
+    )
 }
 
 export default Community
