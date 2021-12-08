fa9aff464f86e6378bfe14662cb1d595e19deb98
Ujjwal Kumar <epicujjwal@protonmail.com>
2021-12-08 09:48:36 +0530

    about: add goal

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,7 +2,18 @@ import { Button } from "@chakra-ui/react"
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { FadeInDown } from '../animations'
-import { Hero, Container, Heading, Break, Description } from '../styles/pageStyles'
+import {
+  Hero,
+  Container,
+  Heading,
+  Break,
+  Description,
+  Content_Description,
+  Content_Heading,
+  Content,
+  Contents,
+  List
+} from '../styles/pageStyles'
 
 const About: NextPage = () => {
   return (
@@ -10,15 +21,15 @@ const About: NextPage = () => {
       <Head>
         <title>About Us</title>
         <meta
-            name="description"
-            content="About hackarmour, hackarmour CTF and the infosec search engine."
-          />
-          <meta name="title" content="Hack Armour | About Us" />
+          name="description"
+          content="About hackarmour, hackarmour CTF and the infosec search engine."
+        />
+        <meta name="title" content="Hack Armour | About Us" />
       </Head>
       <Hero>
         <FadeInDown>
           <Container>
-            <Heading className="bold-fira" style={{lineHeight: '3.87rem'}}>
+            <Heading className="bold-fira" style={{ lineHeight: '3.87rem' }}>
               About Our Vision
             </Heading>
             <Break />
@@ -34,6 +45,21 @@ const About: NextPage = () => {
           </Container>
         </FadeInDown>
       </Hero>
+      <FadeInDown>
+        <Contents>
+          <Content>
+            <Content_Heading className="bold-fira">The Search Engine</Content_Heading>
+            <br />
+            <Content_Description className="text-2">
+            Our primary goal is to make an outstanding infosec community where people can collaborate and help each other. Currently, we are picking up the best resources available on the internet to index for everyone. We are working on a search engine, which only indexes cybersecurity content from different places like GitHub, Reddit, CVEs, and exploits, aggregating all of them in one place.                <br />
+              <br />
+              <p>
+              Our other goal is to make quality CTF challenges so our members can have fun and learn with the community at the same time. We are also aiming to make some teams where people can collaborate on different projects for example the &quot;malware team&quot; may work together in making malware-based challenges.
+              </p>
+            </Content_Description>
+          </Content>
+        </Contents>
+      </FadeInDown>
     </div>
   )
 }
