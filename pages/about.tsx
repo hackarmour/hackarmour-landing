22506fabf136af81d0e1d7766797e09d0d3e4c4c
Ujjwal Kumar <epicujjwal@protonmail.com>
2021-12-18 23:46:44 +0530

    about: add staff images
    grid, to be replaced by respective members

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@chakra-ui/react"
-import styled from "@emotion/styled"
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import { FadeInDown } from '../animations'
-import Footer from "../components/footer"
+import { Button, SimpleGrid } from "@chakra-ui/react";
+import styled from "@emotion/styled";
+import type { NextPage } from "next";
+import Head from "next/head";
+import { FadeInDown } from "../animations";
+import Footer from "../components/footer";
 import {
   Hero,
   Container,
@@ -16,7 +16,7 @@ import {
   Contents,
   DisplayDesktop,
   DisplayMobile
-} from '../styles/pageStyles'
+} from "../styles/pageStyles";
 
 const About: NextPage = () => {
   return (
@@ -32,12 +32,13 @@ const About: NextPage = () => {
       <Hero>
         <FadeInDown>
           <Container>
-            <Heading className="bold-fira" style={{ lineHeight: '3.87rem' }}>
+            <Heading className="bold-fira" style={{ lineHeight: "3.87rem" }}>
               About Our Vision
             </Heading>
             <Break />
             <Description className="text">
-              We are aimed towards organizing all the infosec resources available.
+              We are aimed towards organizing all the infosec resources
+              available.
             </Description>
             <br />
             <a href="about#contact">
@@ -45,7 +46,9 @@ const About: NextPage = () => {
                 className="text-med"
                 backgroundColor="#2D5BFF"
                 fontSize={{ lg: "1.3rem", md: "1rem" }}
-              >Contact Us</Button>
+              >
+                Contact Us
+              </Button>
             </a>
           </Container>
         </FadeInDown>
@@ -53,13 +56,25 @@ const About: NextPage = () => {
       <FadeInDown>
         <Contents>
           <Content>
-            <Content_Heading className="bold-fira">The Search Engine</Content_Heading>
+            <Content_Heading className="bold-fira">
+              The Search Engine
+            </Content_Heading>
             <br />
             <Content_Description className="text-2">
-              Our primary goal is to make an outstanding infosec community where people can collaborate and help each other. Currently, we are picking up the best resources available on the internet to index for everyone. We are working on a search engine, which only indexes cybersecurity content from different places like GitHub, Reddit, CVEs, and exploits, aggregating all of them in one place.                <br />
+              Our primary goal is to make an outstanding infosec community where
+              people can collaborate and help each other. Currently, we are
+              picking up the best resources available on the internet to index
+              for everyone. We are working on a search engine, which only
+              indexes cybersecurity content from different places like GitHub,
+              Reddit, CVEs, and exploits, aggregating all of them in one place.{" "}
+              <br />
               <br />
               <p>
-                Our other goal is to make quality CTF challenges so our members can have fun and learn with the community at the same time. We are also aiming to make some teams where people can collaborate on different projects for example the &quot;malware team&quot; may work together in making malware-based challenges.
+                Our other goal is to make quality CTF challenges so our members
+                can have fun and learn with the community at the same time. We
+                are also aiming to make some teams where people can collaborate
+                on different projects for example the &quot;malware team&quot;
+                may work together in making malware-based challenges.
               </p>
             </Content_Description>
           </Content>
@@ -67,7 +82,12 @@ const About: NextPage = () => {
           <Content>
             <Content_Heading className="bold-fira">Our Team</Content_Heading>
             <Content_Description className="text-2">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut amet totam natus, consectetur quam excepturi delectus temporibus rem dolore dignissimos soluta nemo incidunt, inventore dolores maiores libero officiis accusamus ad hic magnam quia repellat voluptatibus beatae. Similique consequatur, quas rerum quasi autem, cupiditate, pariatur magnam sunt natus voluptate fugiat est.
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut amet
+              totam natus, consectetur quam excepturi delectus temporibus rem
+              dolore dignissimos soluta nemo incidunt, inventore dolores maiores
+              libero officiis accusamus ad hic magnam quia repellat voluptatibus
+              beatae. Similique consequatur, quas rerum quasi autem, cupiditate,
+              pariatur magnam sunt natus voluptate fugiat est.
             </Content_Description>
             <br />
             <MainStaff>
@@ -75,9 +95,13 @@ const About: NextPage = () => {
                 <img src="./ujjwal.png" alt="alt" />
               </MainImageGrid>
               <MainContentGrid>
-                <MainStaffHeading className="text-2">@ujjwal-kr - Founder</MainStaffHeading>
+                <MainStaffHeading className="text-2">
+                  @ujjwal-kr - Founder
+                </MainStaffHeading>
                 <Content_Description className="text-2">
-                  Lead of the Engineering and Product teams of the search engine and some opensource software released. Likes web dev, system programming and reverse engineering.
+                  Lead of the Engineering and Product teams of the search engine
+                  and some opensource software released. Likes web dev, system
+                  programming and reverse engineering.
                 </Content_Description>
               </MainContentGrid>
             </MainStaff>
@@ -85,9 +109,13 @@ const About: NextPage = () => {
             <DisplayDesktop>
               <MainStaff>
                 <MainContentGrid>
-                  <MainStaffHeading className="text-2">@0xGamer - CEO</MainStaffHeading>
+                  <MainStaffHeading className="text-2">
+                    @0xGamer - CEO
+                  </MainStaffHeading>
                   <Content_Description className="text-2">
-                    CEO, operations manager and Discord Admin. Organizer and challenge creator of CTFs and some infosec tools released, enjoys pentesting and pwning boxes.
+                    CEO, operations manager and Discord Admin. Organizer and
+                    challenge creator of CTFs and some infosec tools released,
+                    enjoys pentesting and pwning boxes.
                   </Content_Description>
                 </MainContentGrid>
                 <MainImageGrid>
@@ -102,22 +130,62 @@ const About: NextPage = () => {
                   <img src="./gamer.png" alt="alt" />
                 </MainImageGrid>
                 <MainContentGrid>
-                  <MainStaffHeading className="text-2">@0xGamer - CEO</MainStaffHeading>
+                  <MainStaffHeading className="text-2">
+                    @0xGamer - CEO
+                  </MainStaffHeading>
                   <Content_Description className="text-2">
-                    CEO, operations manager and Discord Admin. Organizer and challenge creator of CTFs and some infosec tools released, enjoys pentesting and pwning boxes.
+                    CEO, operations manager and Discord Admin. Organizer and
+                    challenge creator of CTFs and some infosec tools released,
+                    enjoys pentesting and pwning boxes.
                   </Content_Description>
                 </MainContentGrid>
               </MainStaff>
             </DisplayMobile>
-
-
+          </Content>
+          <Content style={{ textAlign: "center" }}>
+            <br />
+            <Content_Heading className="bold-fira">
+              Staff Members
+            </Content_Heading>
+            <br />
+            <div
+              style={{
+                width: "100%",
+                display: "flex",
+                justifyContent: "center"
+              }}
+            >
+              <SimpleGrid
+                columns={{ base: 2, lg: 3 }}
+                gap={{ base: 5, lg: 10 }}
+              >
+                <a href="https://github.com/ujjwal-kr">
+                  <StaffImg src="./ujjwal.png" alt="staff" />
+                </a>{" "}
+                <a href="https://github.com/ujjwal-kr">
+                  <StaffImg src="./ujjwal.png" alt="staff" />
+                </a>{" "}
+                <a href="https://github.com/ujjwal-kr">
+                  <StaffImg src="./ujjwal.png" alt="staff" />
+                </a>{" "}
+                <a href="https://github.com/ujjwal-kr">
+                  <StaffImg src="./ujjwal.png" alt="staff" />
+                </a>{" "}
+                <a href="https://github.com/ujjwal-kr">
+                  <StaffImg src="./ujjwal.png" alt="staff" />
+                </a>{" "}
+                <a href="https://github.com/ujjwal-kr">
+                  <StaffImg src="./ujjwal.png" alt="staff" />
+                </a>{" "}
+              </SimpleGrid>
+            </div>
           </Content>
         </Contents>
       </FadeInDown>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
 const MainStaff = styled.div`
   margin-top: 3rem;
@@ -135,11 +203,11 @@ const MainStaff = styled.div`
     display: inline;
     text-align: center;
   }
-`
+`;
 
 const MainStaffHeading = styled.h1`
   font-size: 1.5rem;
-`
+`;
 
 const MainImageGrid = styled.div`
   width: 30%;
@@ -149,7 +217,7 @@ const MainImageGrid = styled.div`
     justify-content: center;
     width: 100%;
   }
-`
+`;
 
 const MainContentGrid = styled.div`
   margin: auto;
@@ -159,6 +227,14 @@ const MainContentGrid = styled.div`
     margin-top: 1rem;
     margin-bottom: 1rem;
   }
-`
+`;
 
-export default About
+const StaffImg = styled.img`
+  width: 10rem;
+  height: 10rem;
+  /* @media all and (max-width: 900px) {
+    height: 8.6rem;
+  } */
+`;
+
+export default About;
