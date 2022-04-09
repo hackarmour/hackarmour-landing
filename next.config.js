a460824263038bf22e087de038f5f905a1be46fe
0xGamer <58077694+0xGamer@users.noreply.github.com>
2022-04-09 15:18:55 +0530

    feat: Added policy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  rewrites: async () => [
+    {
+      source: "/public/policy.html",
+      destination: "/pages/api/policy.js",
+    },
+  ],
 }
