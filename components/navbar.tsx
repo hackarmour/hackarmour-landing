7e59ff083c5d51f4a557b03fe9d2ae24b10c4691
TheEmperor342 <83999665+TheEmperor342@users.noreply.github.com>
2022-05-12 17:33:12 +0530

    ui: newsletter, and footer

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,10 +1,10 @@
 import {
-  SimpleGrid,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  IconButton,
+	SimpleGrid,
+	Menu,
+	MenuButton,
+	MenuList,
+	MenuItem,
+	IconButton,
 } from "@chakra-ui/react";
 import { HamburgerIcon, ExternalLinkIcon } from "@chakra-ui/icons";
 import styled from "@emotion/styled";
@@ -14,164 +14,164 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
 const Navbar = () => {
-  const router = useRouter();
-  const [path, changePath]: any = useState();
-  useEffect(() => {
-    changePath(router.pathname);
-    const handleRouteChange = (url: any) => {
-      changePath(url);
-    };
-    router.events.on("routeChangeStart", handleRouteChange);
-  }, [router.events, router.pathname]);
+	const router = useRouter();
+	const [path, changePath]: any = useState();
+	useEffect(() => {
+		changePath(router.pathname);
+		const handleRouteChange = (url: any) => {
+			changePath(url);
+		};
+		router.events.on("routeChangeStart", handleRouteChange);
+	}, [router.events, router.pathname]);
 
-  let about = false;
-  let ctf = false;
+	let about = false;
+	let ctf = false;
 
-  if (path == "/about") about = true;
-  else if (path == "/ctf") ctf = true;
+	if (path == "/about") about = true;
+	else if (path == "/ctf") ctf = true;
 
-  return (
-    <Nav>
-      <FadeInDown>
-        <SimpleGrid columns={{ base: 2, md: 2, lg: 3 }}>
-          <Link href="/" passHref>
-            <Logo>
-              <img src="./logo.svg" alt="logo" />
-              <strong>
-                <code>HackArmour</code>
-              </strong>
-            </Logo>
-          </Link>
-          <Links className="text-2">
-            <SimpleGrid columns={3} spacing={10}>
-              <a
-                href="https://github.com/hackarmour"
-                target="_blank"
-                rel="noreferrer"
-              >
-                GitHub
-              </a>
-              {ctf ? (
-                <u>
-                  <a
-                    href="https://ctftime.org/event/1622"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    CTFs
-                  </a>
-                </u>
-              ) : (
-                <a
-                  href="https://ctftime.org/event/1622"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  CTFs
-                </a>
-              )}
-              {about ? (
-                <u>
-                  <Link href="/about">About</Link>
-                </u>
-              ) : (
-                <Link href="/about">About</Link>
-              )}
-            </SimpleGrid>
-          </Links>
-          <Discord
-            href="https://discord.gg/mxHtj8q3c4"
-            target="_blank"
-            rel="noreferrer"
-            className="text-2"
-          >
-            Discord
-          </Discord>
-          <MenuC>
-            <Menu>
-              <MenuButton
-                as={IconButton}
-                aria-label="Options"
-                icon={<HamburgerIcon w={6} h={6} />}
-              />
+	return (
+		<Nav>
+			<FadeInDown>
+				<SimpleGrid columns={{ base: 2, md: 2, lg: 3 }}>
+					<Link href="/" passHref>
+						<Logo>
+							<img src="./logo.svg" alt="logo" />
+							<strong>
+								<code>HackArmour</code>
+							</strong>
+						</Logo>
+					</Link>
+					<Links className="text-2">
+						<SimpleGrid columns={3} spacing={10}>
+							<a
+								href="https://github.com/hackarmour"
+								target="_blank"
+								rel="noreferrer"
+							>
+								GitHub
+							</a>
+							{ctf ? (
+								<u>
+									<a
+										href="https://ctftime.org/event/1622"
+										target="_blank"
+										rel="noreferrer"
+									>
+										CTFs
+									</a>
+								</u>
+							) : (
+								<a
+									href="https://ctftime.org/event/1622"
+									target="_blank"
+									rel="noreferrer"
+								>
+									CTFs
+								</a>
+							)}
+							{about ? (
+								<u>
+									<Link href="/about">About</Link>
+								</u>
+							) : (
+								<Link href="/about">About</Link>
+							)}
+						</SimpleGrid>
+					</Links>
+					<Discord
+						href="https://discord.gg/mxHtj8q3c4"
+						target="_blank"
+						rel="noreferrer"
+						className="text-2"
+					>
+						Discord
+					</Discord>
+					<MenuC>
+						<Menu>
+							<MenuButton
+								as={IconButton}
+								aria-label="Options"
+								icon={<HamburgerIcon w={6} h={6} />}
+							/>
 
-              <MenuList className="text-2">
-                <Link href="/" passHref>
-                  <MenuItem>Home</MenuItem>
-                </Link>
-                <Link href="/about" passHref>
-                  <MenuItem>About Us</MenuItem>
-                </Link>
-                <a
-                  href="https://ctftime.org/event/1622"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <MenuItem icon={<ExternalLinkIcon />}>CTF</MenuItem>
-                </a>
-                <a
-                  target="_blank"
-                  rel="noreferrer"
-                  href="https://github.com/hackarmour"
-                >
-                  <MenuItem icon={<ExternalLinkIcon />}>GitHub</MenuItem>
-                </a>
-                <a
-                  target="_blank"
-                  rel="noreferrer"
-                  href="https://discord.gg/mxHtj8q3c4"
-                >
-                  <MenuItem icon={<ExternalLinkIcon />}>Discord</MenuItem>
-                </a>
-              </MenuList>
-            </Menu>
-          </MenuC>
-        </SimpleGrid>
-      </FadeInDown>
-    </Nav>
-  );
+							<MenuList className="text-2">
+								<Link href="/" passHref>
+									<MenuItem>Home</MenuItem>
+								</Link>
+								<Link href="/about" passHref>
+									<MenuItem>About Us</MenuItem>
+								</Link>
+								<a
+									href="https://ctftime.org/event/1622"
+									target="_blank"
+									rel="noreferrer"
+								>
+									<MenuItem icon={<ExternalLinkIcon />}>CTF</MenuItem>
+								</a>
+								<a
+									target="_blank"
+									rel="noreferrer"
+									href="https://github.com/hackarmour"
+								>
+									<MenuItem icon={<ExternalLinkIcon />}>GitHub</MenuItem>
+								</a>
+								<a
+									target="_blank"
+									rel="noreferrer"
+									href="https://discord.gg/mxHtj8q3c4"
+								>
+									<MenuItem icon={<ExternalLinkIcon />}>Discord</MenuItem>
+								</a>
+							</MenuList>
+						</Menu>
+					</MenuC>
+				</SimpleGrid>
+			</FadeInDown>
+		</Nav>
+	);
 };
 
 const Nav = styled.nav`
-  padding: 0.5rem;
-  font-size: 1.12rem;
+	padding: 0.5rem;
+	font-size: 1.12rem;
 `;
 
 const Links = styled.div`
-  text-align: center;
-  font-weight: bold;
-  margin: auto;
-  @media all and (max-width: 62rem) {
-    display: none !important;
-  }
+	text-align: center;
+	font-weight: bold;
+	margin: auto;
+	@media all and (max-width: 62rem) {
+		display: none !important;
+	}
 `;
 
 const Discord = styled.a`
-  text-align: right;
-  margin: auto;
-  @media all and (max-width: 62rem) {
-    display: none !important;
-  }
+	text-align: right;
+	margin: auto;
+	@media all and (max-width: 62rem) {
+		display: none !important;
+	}
 `;
 
 const Logo = styled.div`
-  font-size: 1.4rem;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  cursor: pointer;
+	font-size: 1.4rem;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	cursor: pointer;
 `;
 
 const MenuC = styled.div`
-  display: flex;
-  justify-content: right;
-  margin-top: auto;
-  align-items: center;
-  height: 100%;
-  width: 100%;
-  @media all and (min-width: 62rem) {
-    display: none !important;
-  }
+	display: flex;
+	justify-content: right;
+	margin-top: auto;
+	align-items: center;
+	height: 100%;
+	width: 100%;
+	@media all and (min-width: 62rem) {
+		display: none !important;
+	}
 `;
 
 export default Navbar;
--- pages/newsletter.tsx
+++ pages/newsletter.tsx
@@ -1,132 +1,129 @@
 import { useRef, useState } from "react";
 import {
-  Input,
-  Button,
-  Alert,
-  AlertDescription,
-  CloseButton,
+	Input,
+	Button,
+	Alert,
+	AlertDescription,
+	CloseButton,
+	Flex,
 } from "@chakra-ui/react";
 import styled from "@emotion/styled";
 import Head from "next/head";
 import { db } from "../firebase";
+import Footer from "../components/footer";
 export default function NewsLetter() {
-  const emailRef = useRef(null);
-  const userNameRef = useRef(null);
-  const [shown, setShown] = useState(false);
-  const handleSubmit = async () => {
-    const email = (emailRef.current! as any).value;
-    const userName = (userNameRef.current! as any).value;
-    await db.collection("newsletter").add({
-      email,
-      userName,
-      createdAt: new Date(),
-    });
-    (emailRef.current! as any).value = "";
-    (userNameRef.current! as any).value = "";
-    setShown(true);
-  };
-  return (
-    <>
-      <Head>
-        <title>Newsletter</title>
-        <meta
-          name="description"
-          content="
+	const emailRef = useRef(null);
+	const userNameRef = useRef(null);
+	const [shown, setShown] = useState(false);
+	const handleSubmit = async () => {
+		const email = (emailRef.current! as any).value;
+		const userName = (userNameRef.current! as any).value;
+		await db.collection("newsletter").add({
+			email,
+			userName,
+			createdAt: new Date(),
+		});
+		(emailRef.current! as any).value = "";
+		(userNameRef.current! as any).value = "";
+		setShown(true);
+	};
+	return (
+		<>
+			<Head>
+				<title>Newsletter</title>
+				<meta
+					name="description"
+					content="
         Subscribe to our newsletter to get the latest news and updates.
         "
-        />
-        <meta name="title" content="Newsletter" />
-      </Head>
-      {shown && (
-        <Alert status="success">
-          <AlertDescription>
-            <h1
-              style={{
-                fontWeight: "bold",
-                textAlign: "center",
-                fontFamily: "'Josefin Sans', sans-serif",
-              }}
-            >
-              Thank you for subscribing!
-            </h1>
-          </AlertDescription>
-          <CloseButton
-            alignSelf="flex-start"
-            position="relative"
-            right={-1}
-            top={-1}
-            onClick={() => setShown(!shown)}
-          />
-        </Alert>
-      )}
-      <Centered>
-        <ModalContainer>
-          <h3
-            style={{
-              fontFamily: "'Josefin Sans'",
-              fontSize: "1.4rem",
-              textAlign: "center",
-            }}
-          >
-            Get Updates About The CTF Awards
-          </h3>
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              handleSubmit();
-            }}
-          >
-            <Input
-              type="text"
-              required
-              placeholder="Your Name"
-              ref={userNameRef}
-              autoComplete="name"
-            />
-            <Input
-              type="email"
-              required
-              placeholder="Your Email"
-              ref={emailRef}
-              autoComplete="email"
-            />
-            <Button type="submit">Submit</Button>
-          </form>
-        </ModalContainer>
-      </Centered>
-    </>
-  );
+				/>
+				<meta name="title" content="Newsletter" />
+			</Head>
+			{shown && (
+				<Alert status="success">
+					<AlertDescription>
+						<h1
+							style={{
+								fontWeight: "bold",
+								textAlign: "center",
+								fontFamily: "'Josefin Sans', sans-serif",
+							}}
+						>
+							Thank you for subscribing!
+						</h1>
+					</AlertDescription>
+					<CloseButton
+						alignSelf="flex-start"
+						position="relative"
+						right={-1}
+						top={-1}
+						onClick={() => setShown(!shown)}
+					/>
+				</Alert>
+			)}
+			<Flex direction="column" alignItems="center" justifyContent="center">
+				<ModalContainer>
+					<h3
+						style={{
+							fontSize: "1.4rem",
+							textAlign: "center",
+							marginBottom: "10px",
+						}}
+					>
+						Get Updates About The CTF Awards
+					</h3>
+					<form
+						onSubmit={(e) => {
+							e.preventDefault();
+							handleSubmit();
+						}}
+					>
+						<Input
+							type="text"
+							required
+							placeholder="Your Name"
+							ref={userNameRef}
+							autoComplete="name"
+						/>
+						<Input
+							type="email"
+							required
+							placeholder="Your Email"
+							ref={emailRef}
+							autoComplete="email"
+						/>
+						<Button className="text-med">Submit</Button>
+					</form>
+				</ModalContainer>
+			</Flex>
+			<Footer />
+		</>
+	);
 }
 
-const Container = styled.div`
-  display: flex;
-  flex: 1;
-`;
-
-const Centered = styled(Container)`
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`;
-
-const ModalContainer = styled(Centered)`
-  border: 1px solid #ccc;
-  border-radius: 10px;
-  box-shadow: 0px 0px 10px #ccc;
-  width: 50%;
-  padding: 1rem;
-  margin-top: 20vh;
-  @media (min-width: 570px) and (max-width: 767px) {
-    width: 70%;
-  }
-  @media (max-width: 400px) {
-    width: 90%;
-  }
-  & form {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    gap: 1rem;
-  }
+const ModalContainer = styled.div`
+	display: flex;
+	flex: 1;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	border: 1px solid rgba(255, 255, 255, 0.4);
+	border-radius: 5px;
+	box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.4);
+	width: 50%;
+	padding: 1rem;
+	margin-top: 20vh;
+	@media (min-width: 570px) and (max-width: 767px) {
+		width: 70%;
+	}
+	@media (max-width: 400px) {
+		width: 90%;
+	}
+	& form {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		gap: 1rem;
+	}
 `;
