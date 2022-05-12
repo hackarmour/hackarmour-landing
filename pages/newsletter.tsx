74b94f7bb92d59f675f3b39dbf18510c81cfeb27
TheEmperor342 <83999665+TheEmperor342@users.noreply.github.com>
2022-05-12 19:11:24 +0530

    ui: update ui

--- pages/newsletter.tsx
+++ pages/newsletter.tsx
@@ -6,15 +6,22 @@ import {
 	AlertDescription,
 	CloseButton,
 	Flex,
+	Text,
+	Image,
+	useMediaQuery,
 } from "@chakra-ui/react";
 import styled from "@emotion/styled";
 import Head from "next/head";
 import { db } from "../firebase";
 import Footer from "../components/footer";
+import { FadeInDown } from "../animations";
+
 export default function NewsLetter() {
 	const emailRef = useRef(null);
 	const userNameRef = useRef(null);
 	const [shown, setShown] = useState(false);
+	const [screenSize] = useMediaQuery("screen and (max-width: 1000px)");
+
 	const handleSubmit = async () => {
 		const email = (emailRef.current! as any).value;
 		const userName = (userNameRef.current! as any).value;
@@ -39,64 +46,112 @@ export default function NewsLetter() {
 				/>
 				<meta name="title" content="Newsletter" />
 			</Head>
-			{shown && (
-				<Alert status="success">
-					<AlertDescription>
-						<h1
-							style={{
-								fontWeight: "bold",
-								textAlign: "center",
-								fontFamily: "'Josefin Sans', sans-serif",
-							}}
+			<FadeInDown>
+				{shown && (
+					<Alert status="success">
+						<AlertDescription>
+							<h1
+								style={{
+									fontWeight: "bold",
+									textAlign: "center",
+								}}
+							>
+								Thank you for subscribing!
+							</h1>
+						</AlertDescription>
+						<CloseButton
+							alignSelf="flex-start"
+							position="relative"
+							right={-1}
+							top={-1}
+							onClick={() => setShown(!shown)}
+						/>
+					</Alert>
+				)}
+				<Flex alignItems="center" justifyContent="center">
+					<Flex alignItems="center" justifyContent="center" w="75vw">
+						<Image
+							src="./logo.svg"
+							w="15rem"
+							display={screenSize ? "none" : "block"}
+						/>
+						<Flex
+							direction="column"
+							alignItems="center"
+							justifyContent="center"
 						>
-							Thank you for subscribing!
-						</h1>
-					</AlertDescription>
-					<CloseButton
-						alignSelf="flex-start"
-						position="relative"
-						right={-1}
-						top={-1}
-						onClick={() => setShown(!shown)}
-					/>
-				</Alert>
-			)}
-			<Flex direction="column" alignItems="center" justifyContent="center">
-				<ModalContainer>
-					<h3
-						style={{
-							fontSize: "1.4rem",
-							textAlign: "center",
-							marginBottom: "10px",
-						}}
-					>
-						Get Updates About The CTF Awards
-					</h3>
-					<form
-						onSubmit={(e) => {
-							e.preventDefault();
-							handleSubmit();
-						}}
-					>
-						<Input
-							type="text"
-							required
-							placeholder="Your Name"
-							ref={userNameRef}
-							autoComplete="name"
-						/>
-						<Input
-							type="email"
-							required
-							placeholder="Your Email"
-							ref={emailRef}
-							autoComplete="email"
-						/>
-						<Button className="text-med">Submit</Button>
-					</form>
-				</ModalContainer>
-			</Flex>
-			<Footer />
+							<ModalContainer>
+								<h3
+									style={{
+										fontWeight: "bold",
+										fontSize: "1.4rem",
+										textAlign: "center",
+										marginBottom: "10px",
+									}}
+								>
+									Get Updates About The CTF Awards
+								</h3>
+								<form
+									onSubmit={(e) => {
+										e.preventDefault();
+										handleSubmit();
+									}}
+								>
+									<Flex
+										alignItems="center"
+										justifyContent="center"
+										flexDirection={screenSize ? "column" : "row"}
+									>
+										<div>
+											<Input
+												type="text"
+												required
+												placeholder="Your Name"
+												ref={userNameRef}
+												autoComplete="name"
+												mb="5px"
+												mr={screenSize ? "0px" : "5px"}
+											/>
+											<Input
+												type="email"
+												required
+												placeholder="Your Email"
+												ref={emailRef}
+												autoComplete="email"
+												mr={screenSize ? "0px" : "5px"}
+											/>
+										</div>
+										<div>
+											<Input
+												type="text"
+												required
+												placeholder="CTF Username"
+												ref={emailRef}
+												mb="5px"
+												ml={screenSize ? "0px" : "5px"}
+												mt={screenSize ? "5px" : "0px"}
+											/>
+											<Input
+												type="text"
+												required
+												placeholder="CTF Team Name"
+												ref={emailRef}
+												ml={screenSize ? "0px" : "5px"}
+											/>
+										</div>
+									</Flex>
+									<Button className="text-med">Submit</Button>
+								</form>
+							</ModalContainer>
+						</Flex>
+					</Flex>
+				</Flex>
+				<Text color="gray.400" w="100%" textAlign="center">
+					<b>Note</b>: You will only receive your CTF awards if you register
+					here
+				</Text>
+				<Footer />
+			</FadeInDown>
 		</>
 	);
 }
@@ -107,12 +162,9 @@ const ModalContainer = styled.div`
 	flex-direction: column;
 	align-items: center;
 	justify-content: center;
-	border: 1px solid rgba(255, 255, 255, 0.4);
-	border-radius: 5px;
-	box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.4);
 	width: 50%;
-	padding: 1rem;
-	margin-top: 20vh;
+	margin-top: 10rem;
+	margin-bottom: 10rem;
 	@media (max-width: 550px) {
 		width: 90%;
 	}
